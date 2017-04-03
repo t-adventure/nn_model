97fb18319056b034bd9e908a4203c2bcaa60a467
t-adventure <trf.adventure@gmail.com>
2017-04-03 23:22:03 +0900

    changed
    changed, 170403

--- nn_ver1/nn/setting_data.py
+++ nn_ver1/nn/setting_data.py
@@ -1,37 +1,47 @@
 # coding: utf-8
 import numpy as np
 
-all_temp_C = np.loadtxt('all_temp.csv', delimiter=',') # 1122*88
-all_heat_W = np.loadtxt('all_heat.csv', delimiter=',') # 1122*88
 
-all_temp_K = all_temp_C + np.ones(all_temp_C.shape) * 273
+def setting_data(delta):
 
-temp_diff = np.zeros((all_temp_K.shape[0], int(all_temp_K.shape[1] * (all_temp_K.shape[1] - 1)/2)))
-temp_diff_4 = np.zeros(temp_diff.shape)
-h = max(all_temp_K.shape[0], all_heat_W.shape[0])
-N = all_temp_K.shape[1]
-O = temp_diff.shape[1]
-P = all_heat_W.shape[1]
-Total_data = np.zeros((h, int(O + O + P)))
+    all_temp_C = np.loadtxt('all_temp.csv', delimiter=',') # 1122*88
+    all_heat_W = np.loadtxt('all_heat.csv', delimiter=',') # 1122*88
 
-print(N)
+    all_temp_K = all_temp_C + np.ones(all_temp_C.shape) * 273
 
-print(Total_data.shape)
+    temp_diff = np.zeros((all_temp_K.shape[0], int(all_temp_K.shape[1] * (all_temp_K.shape[1] - 1)/2)))
+    temp_diff_4 = np.zeros(temp_diff.shape)
+    h = max(all_temp_K.shape[0], all_heat_W.shape[0])
+    N = all_temp_K.shape[1]
+    O = temp_diff.shape[1]
+    P = all_heat_W.shape[1]
+    Total_data = np.zeros((h, int(O + O + P)))
+    #delta_temp = np.zeros((int(all_temp_K.shape[0] - delta), all_temp_K.shape[1]))
+    delta_temp = np.zeros((int(all_temp_K.shape[0]), all_temp_K.shape[1]))
 
-for i in range(N):
-    for j in range(i + 1, N):
-        #print(N)
-        #print(i)
-        temp_diff[:, int(N * i - i * (1 + i) / 2 + j - i - 1)] = all_temp_K[:, i] - all_temp_K[:, j]
-        temp_diff_4[:, int(N * i - i * (1 + i) / 2 + j - i - 1)] = all_temp_K[:, i]**4 - all_temp_K[:, j]**4
+    for i in range(N):
+        for j in range(i + 1, N):
+            #print(N)
+            #print(i)
+            temp_diff[:, int(N * i - i * (1 + i) / 2 + j - i - 1)] = all_temp_K[:, i] - all_temp_K[:, j]
+            temp_diff_4[:, int(N * i - i * (1 + i) / 2 + j - i - 1)] = all_temp_K[:, i]**4 - all_temp_K[:, j]**4
 
-for i in range(N):
-    Total_data[:, i] = temp_diff[:, i]
+    for i in range(N):
+        Total_data[:, i] = temp_diff[:, i]
 
-for i in range(N):
-    Total_data[:, N + i] = temp_diff_4[:, i]
+    for i in range(N):
+        Total_data[:, N + i] = temp_diff_4[:, i]
 
-for i in range(P):
-    Total_data[:, N + N + i] = all_heat_W[:, i]
+    for i in range(P):
+        Total_data[:, N + N + i] = all_heat_W[:, i]
 
-np.savetxt('Total_data.csv', Total_data, delimiter=", ")
+    np.savetxt('Total_data.csv', Total_data, delimiter=", ")
+
+    for i in range(int(Total_data.shape[0] - delta)):
+        delta_temp[i, :] = all_temp_K[int(i + delta), :] - all_temp_K[i, :]
+
+    return Total_data, delta_temp
+
+if __name__ == '__main__':
+    delta = 300 / 20 # delta_data sampling rate
+    a, b = setting_data(delta) # a:(1122, 7744), b:(1107, 88)
--- nn_ver1/nn/train_neuralnet.py
+++ nn_ver1/nn/train_neuralnet.py
@@ -3,33 +3,34 @@ import sys, os
 sys.path.append(os.pardir)
 
 import numpy as np
-#from dataset.mnist import load_mnist
 from two_layer_net import TwoLayerNet
 from common.optimizer import *
+from setting_data import *
 
 # データの読み込み
-#(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+delta = 300 / 20 # delta_data sampling rate
+x_train, t_train = setting_data(delta)
 
-x_train = np.loadtxt('temp.csv', delimiter=',')
-t_train = np.loadtxt('delta_temp.csv', delimiter=',')
-x_test = np.loadtxt('temp.csv', delimiter=',')
-t_test = np.loadtxt('delta_temp.csv', delimiter=',')
+x_test = x_train
+t_test = t_train
 
 x_train = np.mat(x_train)
 t_train = np.mat(t_train)
 x_test = np.mat(x_test)
 t_test = np.mat(t_test)
-#t_train = t_train.T
-#t_test = t_test.T
 
-network = TwoLayerNet(input_size=23, hidden_size=100, output_size=23)
-optimizer = SGD(lr=0.01)
+input_size = x_train.shape[1]
+output_size = t_train.shape[1]
+hidden_size = 200
 
-iters_num = 100
+network = TwoLayerNet(input_size, hidden_size, output_size)
+optimizer = SGD(lr=0.00000000001)
+
+#iters_num = min(x_train.shape[0], t_train.shape[0])
+iters_num = 20
+
 train_size = x_train.shape[0]
-#print(train_size) #100
 batch_size = 1
-learning_rate = 0.1
 
 train_loss_list = []
 train_acc_list = []
@@ -40,30 +41,21 @@ iter_per_epoch = 1
 
 for i in range(iters_num):
     batch_mask = np.random.choice(train_size, batch_size)
-    #print(x_train[batch_mask])
-    #print(x_train)
-    #print(t_train[2, 0])
     x_batch = x_train[batch_mask, :]
-    #t_batch = t_train[batch_mask, 0]
     t_batch = t_train[batch_mask, :]
     print(i)
     #print(x_batch)
     #print(t_batch)
-    
+
     # 勾配
-    #grad = network.numerical_gradient(x_batch, t_batch)
     grad = network.gradient(x_batch, t_batch)
     params = network.params
     optimizer.update(params, grad)
 
     # 更新
-    #for key in ('W1', 'b1', 'W2', 'b2'):
-        
-        #network.params[key] -= learning_rate * grad[key]
-    
     loss = network.loss(x_batch, t_batch)
     train_loss_list.append(loss)
-    
+
     if i % iter_per_epoch == 0:
         train_acc = network.accuracy(x_train, t_train)
         test_acc = network.accuracy(x_test, t_test)
